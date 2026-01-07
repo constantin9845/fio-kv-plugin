d2b237a075403e813c64aa2222a932d484cc416a
beaver <constantin.y@outlook.com>
2026-01-08 08:55:08 +0900

    make random key sizes only power of 2 numbers

--- app/fio_plugin/include/var_key.h
+++ app/fio_plugin/include/var_key.h
@@ -13,18 +13,44 @@ extern double LARGE_KEY_COUNTER;
 // 4 to 255 bytes
 static inline int get_kv_key_size(double kd){
 
-	// small keys = 4 ~ 32 bytes
-	// large keys = 33 ~ 255 bytes
+	// small keys = 4 ~ 32 bytes [4 8 16 32]
+	// large keys = 33 ~ 255 bytes [64 128]
 
 	int k;
 
 	if(KEY_DISTRIBUTION_STATUS < kd){
 		LARGE_KEY_COUNTER++;
-		k = (4 + rand() % (254 - 33 + 1));
+		int min = 1; 
+		int max = 2;
+		k = (rand() % (max - min + 1)) + min;
+		switch(k){
+			case 1:
+				k = 64;
+				break;
+			case 2:
+				k = 128;
+				break;
+		}
 	}
 	else{
 		SMALL_KEY_COUNTER++;
-		k = (4 + rand() % (32 - 4 + 1));
+		int min = 1; 
+		int max = 4;
+		k = (rand() % (max - min + 1)) + min;
+		switch(k){
+			case 1:
+				k = 4;
+				break;
+			case 2:
+				k = 8;
+				break;
+			case 3:
+				k = 16;
+				break;
+			case 4:
+				k = 32;
+				break;
+		}
 	}
 
 	if(SMALL_KEY_COUNTER == 0)
