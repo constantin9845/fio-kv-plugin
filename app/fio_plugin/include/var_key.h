e30c05c65b45a5fae377e3414946570d1766ab0b
beaver <constantin.y@outlook.com>
2026-01-08 09:52:35 +0900

    128 byte generator for key expansion

--- app/fio_plugin/include/var_key.h
+++ app/fio_plugin/include/var_key.h
@@ -61,8 +61,8 @@ static inline int get_kv_key_size(double kd){
 	return k;
 }
 
-static inline uint64_t splitmix64(u_int64_t *x){
-	uint64_t z = (*x += 0x9e3779b97f4a7c15ULL);
+static inline u_int64_t splitmix64(u_int64_t *x){
+	u_int64_t z = (*x += 0x9e3779b97f4a7c15ULL);
 	z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9ULL;
     z = (z ^ (z >> 27)) * 0x94d049bb133111ebULL;
 	return z ^ (z >> 31);
