a0e9a8ff501d2821e607816800d74c9b497d82f8
beaver <constantin.y@outlook.com>
2026-01-30 11:35:36 +0900

    added scriptfile + workloads

--- app/fio_plugin/full_run.sh
+++ app/fio_plugin/full_run.sh
@@ -2,6 +2,7 @@
 
 HUGE_PAGES=1024
 
+
 cleanup_memory(){
 	echo "*** Clean SPDK environment ***"
 
@@ -30,6 +31,45 @@ cleanup_memory(){
 	sudo ipcs -m | awk '$6==0 {print $2}' | xargs -r sudo ipcrm -m 2>/dev/null
 }
 
+usage() {
+    echo "Usage: $0 -r [1-6] -d [uniform|zipf07|zipf099|zipf12|pareto07|pareto08|pareto09|normal5|normal25|normal50]"
+    exit 1
+}
+
+while getopts ":r:d:" opt; do
+  case $opt in
+    r) 
+		if [[ "$OPTARG" =~ ^[1-6]$ ]]; then
+			RATIO="$OPTARG"
+		else
+			echo "Error: Value Ratio must be between 1 and 6."
+			usage
+		fi
+		;;
+	d)
+		case "$OPTARG" in
+			uniform|zipf07|zipf099|zipf12|pareto07|pareto08|pareto09|normal5|normal25|normal50) DIST="$OPTARG" ;;
+			*) echo "Error: Invalid distribution '$OPTARG'."; usage ;;
+		esac
+		;;
+	\?)
+      	echo "Error: Invalid option -$OPTARG"
+      	usage
+      	;;
+	:)
+      	echo "Error: Option -$OPTARG requires an argument."
+      	usage
+      	;;
+  esac
+done
+
+if [[ -z "$RATIO" || -z "$DIST" ]]; then
+    echo "Error: Both -r and -d are required."
+    usage
+fi
+
+echo "Running Simulation: Ratio $RATIO with $DIST distribution..."
+
 # Uses ASAN optionally to report on segmentation errors
 #FIO_COMMAND="sudo LD_PRELOAD=$(gcc -print-file-name=libasan.so) ASAN_OPTIONS=detect_leaks=0 ./fio-3.3"
 
@@ -38,19 +78,19 @@ FIO_COMMAND="sudo ./fio-3.3"
 
 # *** Phase 1 : Load ***
 sleep 2
-echo "Phase 1: Load\n"
-$FIO_COMMAND load.fio
+echo "Phase 1: Load --> load/W${RATIO}.fio\n"
+$FIO_COMMAND "load/W${RATIO}.fio"
 
 # *** Phase 2 : Warmup ***
 cleanup_memory
 sleep 10
-echo "Phase 2: Warmup\n"
-$FIO_COMMAND warmup.fio
+echo "Phase 2: Warmup --> warmup/W${RATIO}.fio\n"
+$FIO_COMMAND "warmup/W${RATIO}.fio"
 
 # *** Phase 3 : Test run ***
 cleanup_memory
 sleep 60
-echo "Phase 3: Test\n"
-$FIO_COMMAND run.fio
+echo "Phase 3: Test --> run/${DIST}/W${RATIO}.fio\n"
+$FIO_COMMAND "run/${DIST}/W${RATIO}.fio"
 
 cleanup_memory
