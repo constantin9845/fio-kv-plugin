fa6fbb69e962503efd24bd617def266922cdd16f
constantin9845 <constantin.y@outlook.com>
2026-03-05 23:06:13 +0900

    fix type in workload name

--- app/fio_plugin/full_run.sh
+++ app/fio_plugin/full_run.sh
@@ -32,7 +32,7 @@ cleanup_memory(){
 }
 
 usage() {
-    echo "Usage: $0 -r [bimodal|blob_large|blob_small|increasing_small|increasing|small_bimodal] -d [uniform|zipf099|pareto08|normal20]"
+    echo "Usage: $0 -r [bimodal|blob_large|blob_small|increasing_large|increasing|small_bimodal] -d [uniform|zipf099|pareto08|normal20]"
     exit 1
 }
 
@@ -40,7 +40,7 @@ while getopts ":r:d:" opt; do
   case $opt in
     r) 
 		case "$OPTARG" in
-			bimodal|blob_large|blob_small|increasing_small|increasing|small_bimodal) RATIO="$OPTARG" ;;
+			bimodal|blob_large|blob_small|increasing_large|increasing|small_bimodal) RATIO="$OPTARG" ;;
 			*) echo "Error: Invalid distribution '$OPTARG'."; usage ;;
 		esac
 		;;
