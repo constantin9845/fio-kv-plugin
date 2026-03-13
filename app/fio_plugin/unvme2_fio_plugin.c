a24c763d82e0e6d869d127d47ecb4bae18d86175
constantin9845 <constantin.y@outlook.com>
2026-03-14 01:39:19 +0900

    remove alignment

--- app/fio_plugin/unvme2_fio_plugin.c
+++ app/fio_plugin/unvme2_fio_plugin.c
@@ -850,17 +850,9 @@ static int kv_fio_queue(struct thread_data *td, struct io_u *io_u)
 	}
 		
 	if(!fio_req->value_buf){
-		/*
 		fio_req->value_buf_size = 8192;
 		fio_req->value_buf = kv_zalloc(fio_req->value_buf_size);
 		if (!fio_req->value_buf) return FIO_Q_BUSY;
-		*/
-
-		fio_req->value_buf_size = 8192;
-		if (posix_memalign((void**)&fio_req->value_buf, 4096, 8192) != 0) {
-			return FIO_Q_BUSY;
-		}
-		//memset(fio_req->value_buf, 0, 8192);
 	}
 
 	//fio_req->value_buf = kv_zalloc(MEM_ALIGN(valueKB, 4));
