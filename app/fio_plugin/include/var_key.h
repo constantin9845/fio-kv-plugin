a5776f9a0297b5c580e471c41240e21c3d981ed5
constantin9845 <constantin.y@outlook.com>
2026-04-02 00:35:33 +0900

    further gap between smalles and largest value sizes

--- app/fio_plugin/include/var_key.h
+++ app/fio_plugin/include/var_key.h
@@ -147,7 +147,7 @@ static inline u_int32_t get_kv_value_size(u_int64_t prob, bool is_read){
 		else{
 			COUNTER_WRITE_128++;
 		}
-		return (u_int32_t)32; 
+		return (u_int32_t)64; 
 	}
 
 	if(prob < (u_int32_t)target_64 + (u_int32_t)target_128 + (u_int32_t)target_256){ 
@@ -157,7 +157,7 @@ static inline u_int32_t get_kv_value_size(u_int64_t prob, bool is_read){
 		else{
 			COUNTER_WRITE_256++;
 		}
-		return (u_int32_t)128; 
+		return (u_int32_t)512; 
 	}
 
 	if(prob < (u_int32_t)target_64 + (u_int32_t)target_128 + (u_int32_t)target_256 + (u_int32_t)target_512){ 
@@ -167,7 +167,7 @@ static inline u_int32_t get_kv_value_size(u_int64_t prob, bool is_read){
 		else{
 			COUNTER_WRITE_512++;
 		}
-		return (u_int32_t)256; 
+		return (u_int32_t)1024; 
 	}
 
 	if(is_read){
