80171470ecc0dc9942bda7e67759540333bba478
beaver <beaver@mail.com>
2026-04-07 15:06:14 +0900

    test extreme value size contrast

--- app/fio_plugin/include/var_key.h
+++ app/fio_plugin/include/var_key.h
@@ -147,7 +147,7 @@ static inline u_int32_t get_kv_value_size(u_int64_t prob, bool is_read){
 		else{
 			COUNTER_WRITE_128++;
 		}
-		return (u_int32_t)64; 
+		return (u_int32_t)32; 
 	}
 
 	if(prob < (u_int32_t)target_64 + (u_int32_t)target_128 + (u_int32_t)target_256){ 
@@ -157,7 +157,7 @@ static inline u_int32_t get_kv_value_size(u_int64_t prob, bool is_read){
 		else{
 			COUNTER_WRITE_256++;
 		}
-		return (u_int32_t)128; 
+		return (u_int32_t)64; 
 	}
 
 	if(prob < (u_int32_t)target_64 + (u_int32_t)target_128 + (u_int32_t)target_256 + (u_int32_t)target_512){ 
@@ -167,7 +167,7 @@ static inline u_int32_t get_kv_value_size(u_int64_t prob, bool is_read){
 		else{
 			COUNTER_WRITE_512++;
 		}
-		return (u_int32_t)1024; 
+		return (u_int32_t)256; 
 	}
 
 	if(is_read){
@@ -176,7 +176,7 @@ static inline u_int32_t get_kv_value_size(u_int64_t prob, bool is_read){
 	else{
 		COUNTER_WRITE_1024++;
 	}
-	return (u_int32_t)2048;
+	return (u_int32_t)4096;
 	
 }
 
