127268429092ac80037d435e2c55c05f92c96a5a
beaver <beaver@mail.com>
2026-09-07 14:06:39 +0900

    test size

--- app/fio_plugin/include/var_key.h
+++ app/fio_plugin/include/var_key.h
@@ -136,7 +136,7 @@ static inline u_int32_t get_kv_value_size(u_int64_t prob, bool is_read){
 		else{
 			COUNTER_WRITE_64++;
 		}
-		return (u_int32_t)32; 
+		return (u_int32_t)16; 
 	}
 
 	
@@ -167,7 +167,7 @@ static inline u_int32_t get_kv_value_size(u_int64_t prob, bool is_read){
 		else{
 			COUNTER_WRITE_512++;
 		}
-		return (u_int32_t)1024; 
+		return (u_int32_t)820; 
 	}
 
 	if(is_read){
@@ -176,7 +176,7 @@ static inline u_int32_t get_kv_value_size(u_int64_t prob, bool is_read){
 	else{
 		COUNTER_WRITE_1024++;
 	}
-	return (u_int32_t)2048;
+	return (u_int32_t)1024;
 	
 }
 
