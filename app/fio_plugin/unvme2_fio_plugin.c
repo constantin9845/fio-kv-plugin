cd696127b27e420ead58bd33df640ec85e67b76a
constantin9845 <constantin.y@outlook.com>
2026-09-16 22:07:58 +0900

    compiler errors

--- app/fio_plugin/unvme2_fio_plugin.c
+++ app/fio_plugin/unvme2_fio_plugin.c
@@ -129,7 +129,6 @@ static struct fio_option options[] = {
 				.def	= "32_100",
 				.help	= "Example: key_ratio=8.16.32.64.128.256_10.20.20.20.20.10 --> key ratio={size.size..}_{ratio.ratio..} (max 6)",
 				.category = FIO_OPT_C_ENGINE,
-				.category = FIO_OPT_C_ENGINE,
 		},
         {
                 .name   = "ks",
@@ -931,29 +930,6 @@ static int kv_fio_queue(struct thread_data *td, struct io_u *io_u)
 		//break;
 		return FIO_Q_COMPLETED;
 	}
-
-	
-	//printf("Command completion code = %d\n", ret);
-	/*
-	if(io_u->ddir == DDIR_READ){
-		printf("Current Ratios: READS\n");
-		printf("Keys:\n");
-		printf("4   : %.2f | %.2f\n", KEY_COUNTER_READ_4, (KEY_COUNTER_READ_4/IO_COUNTER_READ)*100);
-		printf("8   : %.2f | %.2f\n", KEY_COUNTER_READ_8, (KEY_COUNTER_READ_8/IO_COUNTER_READ)*100);
-		printf("16  : %.2f | %.2f\n", KEY_COUNTER_READ_16, (KEY_COUNTER_READ_16/IO_COUNTER_READ)*100);
-		printf("32  : %.2f | %.2f\n", KEY_COUNTER_READ_32, (KEY_COUNTER_READ_32/IO_COUNTER_READ)*100);
-		printf("64  : %.2f | %.2f\n", KEY_COUNTER_READ_64, (KEY_COUNTER_READ_64/IO_COUNTER_READ)*100);
-		printf("128 : %.2f | %.2f\n", KEY_COUNTER_READ_128, (KEY_COUNTER_READ_128/IO_COUNTER_READ)*100);
-
-		printf("\n");
-		printf("Values:\n");
-		printf("64    : %.2f | %.2f\n", COUNTER_READ_64, (COUNTER_READ_64/IO_COUNTER_READ)*100);
-		printf("128   : %.2f | %.2f\n", COUNTER_READ_128, (COUNTER_READ_128/IO_COUNTER_READ)*100);
-		printf("256   : %.2f | %.2f\n", COUNTER_READ_256, (COUNTER_READ_256/IO_COUNTER_READ)*100);
-		printf("512   : %.2f | %.2f\n", COUNTER_READ_512, (COUNTER_READ_512/IO_COUNTER_READ)*100);
-		printf("1024  : %.2f | %.2f\n", COUNTER_READ_1024, (COUNTER_READ_1024/IO_COUNTER_READ)*100);
-	}
-	*/
 
 	return (ret) ? FIO_Q_BUSY : fio_thread->fio_q_finished;
         // FIO_Q_COMPLETED = 0, /* completed sync */
