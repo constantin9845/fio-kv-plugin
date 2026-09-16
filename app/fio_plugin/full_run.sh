cf61cc6ee25119e5a44b07b5b4a5b20b6587beea
beaver <beaver@mail.com>
2026-09-16 16:04:06 +0900

    update to dynamic size choices

--- app/fio_plugin/full_run.sh
+++ app/fio_plugin/full_run.sh
@@ -3,7 +3,9 @@
 # Check if all 3 arguments are provided
 if [ "$#" -ne 3 ]; then
     echo "Usage: $0 <size> <value_ratio> <distribution>"
-    echo "Example: $0 1650M 64.76:128.10:256.5 pareto:0.9"
+    echo "key_ratio format  : Example: key_ratio=8.16.32.64.128.256_10.20.20.20.20.10 --> key ratio={size.size..}_{ratio.ratio..} (max 6)"
+    echo "value_ratio format: Example: value_ratio=64.128.256.512.1024_70.15.10.4.1 --> value ratio={size.size..}_{ratio.ratio..} (max 5)"
+    echo "Example: $0 1650M value_ratio=64.128.256.512.1024_70.15.10.4.1 pareto:0.9"
     exit 1
 fi
 
