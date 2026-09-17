e85f3b8ad51e07063236a59aed443ab7b0a040b6
constantin9845 <constantin.y@outlook.com>
2026-09-18 00:30:53 +0900

    logs

--- app/fio_plugin/full.sh
+++ app/fio_plugin/full.sh
@@ -5,14 +5,16 @@ VAR_SIZE=$1
 KEY=$2
 VALUE=$3
 VAR_DIST=$4
+Q_DEPTH=$5
 
-echo "Executing FIO: SIZE=$VAR_SIZE, KEY_RATIO=$KEY, VALUE_RATIO=$VALUE, DIST=$VAR_DIST"
+echo "Executing FIO: SIZE=$VAR_SIZE, KEY_RATIO=$KEY, VALUE_RATIO=$VALUE, DIST=$VAR_DIST", Q=$Q_DEPTH
 sleep 10
 
 sudo SIZE=$VAR_SIZE \
      KEY_RATIO=$KEY \
      VALUE_RATIO=$VALUE \
      DISTRIBUTION=$VAR_DIST \
+     Q=$Q_DEPTH \
      ./fio-3.3 run_anykey/pre.fio
 
 sleep 120
@@ -21,4 +23,5 @@ sudo SIZE=$VAR_SIZE \
      KEY_RATIO=$KEY \
      VALUE_RATIO=$VALUE \
      DISTRIBUTION=$VAR_DIST \
+     Q=$Q_DEPTH \
      ./fio-3.3 run_anykey/workload.fio
