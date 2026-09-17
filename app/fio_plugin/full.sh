ade816c3dd93e22efcdd26fc6c387f3fecd5c91f
beaver <beaver@mail.com>
2026-09-17 14:05:11 +0900

    full run script

--- app/fio_plugin/full.sh
+++ app/fio_plugin/full.sh
@@ -2,17 +2,23 @@
 
 # Assign arguments to variables for clarity
 VAR_SIZE=$1
-VAR_DIST=$2
+KEY=$2
+VALUE=$3
+VAR_DIST=$4
 
-echo "Executing FIO: SIZE=$VAR_SIZE, DIST=$VAR_DIST"
+echo "Executing FIO: SIZE=$VAR_SIZE, KEY_RATIO=$KEY, VALUE_RATIO=$VALUE, DIST=$VAR_DIST"
 sleep 10
 
 sudo SIZE=$VAR_SIZE \
+     KEY_RATIO=$KEY \
+     VALUE_RATIO=$VALUE \
      DISTRIBUTION=$VAR_DIST \
      ./fio-3.3 run_anykey/pre.fio
 
 sleep 120
 
 sudo SIZE=$VAR_SIZE \
+     KEY_RATIO=$KEY \
+     VALUE_RATIO=$VALUE \
      DISTRIBUTION=$VAR_DIST \
      ./fio-3.3 run_anykey/workload.fio
